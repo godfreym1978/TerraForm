91b4a4c571f8f46e19596c5229a3294e5ead88d7
godfreym1978 <airp18ne>
2021-08-29 16:09:48 -0500

    Added new files for output and variables.

--- AWS/TFLearning/SingleInstance/main.tf
+++ AWS/TFLearning/SingleInstance/main.tf
@@ -2,7 +2,16 @@ provider "aws" {
     region = "us-east-2"
 }
 
+locals  {
+    instanceName = "test-instance"
+}
+
 resource "aws_instance" "example-ec2"{
     ami = "ami-028f0daffc74d96ee"
     instance_type = "t2.micro"
+
+    tags = {
+        name = local.instanceName
+    }
+
 }
